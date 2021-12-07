d377645e4e23351116f81db5c15ae04838de7813
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-12-07 23:38:36 +0530

    optimizations

--- aoc2021/lib/aoc2021/day 3/day3.ex
+++ aoc2021/lib/aoc2021/day 3/day3.ex
@@ -5,30 +5,19 @@ defmodule Aoc2021.Day3 do
 
   @doc """
   Aoc2021.Day3.import_file("/Users/akhileshnegi/projects/AOC2021/aoc2021/lib/aoc2021/day 3/day3.txt")
+  2250414
   """
   def import_file(path) do
     {:ok, file_input} = File.read(path)
     process_file(file_input)
   end
 
-  @init %{
-    1 => %{0 => 0, 1 => 0},
-    2 => %{0 => 0, 1 => 0},
-    3 => %{0 => 0, 1 => 0},
-    4 => %{0 => 0, 1 => 0},
-    5 => %{0 => 0, 1 => 0},
-    6 => %{0 => 0, 1 => 0},
-    7 => %{0 => 0, 1 => 0},
-    8 => %{0 => 0, 1 => 0},
-    9 => %{0 => 0, 1 => 0},
-    10 => %{0 => 0, 1 => 0},
-    11 => %{0 => 0, 1 => 0},
-    12 => %{0 => 0, 1 => 0}
-  }
   defp process_file(file_input) do
+    init = 1..12 |> Enum.reduce(%{}, fn x, acc -> Map.put(acc, x, %{0 => 0, 1 => 0}) end)
+
     file_input
     |> String.split("\n", trim: true)
-    |> Enum.reduce(@init, fn bit, acc ->
+    |> Enum.reduce(init, fn bit, acc ->
       process_bits(bit, acc)
     end)
     |> process_gamma_rate()
@@ -44,13 +33,12 @@ defmodule Aoc2021.Day3 do
     |> mirror_bits()
   end
 
-  defp mirror_bits(value) do
+  def mirror_bits(value) do
     bit_value =
-      0..11
-      |> Enum.reduce("", fn key, acc ->
-        mirror_value = if String.at(value, key) == "0", do: "1", else: "0"
-        "#{acc}#{mirror_value}"
-      end)
+      value
+      |> String.replace("0", "2")
+      |> String.replace("1", "0")
+      |> String.replace("2", "1")
       |> String.to_integer(2)
 
     bit_value * String.to_integer(value, 2)
@@ -62,18 +50,11 @@ defmodule Aoc2021.Day3 do
   end
 
   defp process_bits(bit, bit_count) do
-    update_bit_count(bit_count, String.at(bit, 0), 1)
-    |> update_bit_count(String.at(bit, 1), 2)
-    |> update_bit_count(String.at(bit, 2), 3)
-    |> update_bit_count(String.at(bit, 3), 4)
-    |> update_bit_count(String.at(bit, 4), 5)
-    |> update_bit_count(String.at(bit, 5), 6)
-    |> update_bit_count(String.at(bit, 6), 7)
-    |> update_bit_count(String.at(bit, 7), 8)
-    |> update_bit_count(String.at(bit, 8), 9)
-    |> update_bit_count(String.at(bit, 9), 10)
-    |> update_bit_count(String.at(bit, 10), 11)
-    |> update_bit_count(String.at(bit, 11), 12)
+    0..11
+    |> Enum.reduce(bit_count, fn count, acc ->
+      result = update_bit_count(bit_count, String.at(bit, count), count + 1)
+      Map.put(acc, count + 1, result[count + 1])
+    end)
   end
 
   defp update_bit_count(bit_count, value, key) when value == "0" do
